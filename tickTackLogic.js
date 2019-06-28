de18469165cac277663a1c3d00a19125c79841b9
pritampanhale <31956760+pritampanhale@users.noreply.github.com>
2019-06-28 09:23:01 +0530

    Update tickTackLogic.js

--- tickTackLogic.js
+++ tickTackLogic.js
@@ -1,60 +1,115 @@
+var parentElement;
 var app = angular.module('myApp', []);
-app.controller('logicController', function($scope) {
-    $scope.inputArr = ['','','','','','','','',''];
+app.controller('logicController', function ($scope) {
+    var elementArry = new Array();
+    $scope.inputArr = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     $scope.ininialVal = 'O';
-    $scope.letsPlay = ()=>{
+    $scope.letsPlay = () => {
         $scope.playable = true;
     }
-    
-    $scope.addInput = (index)=>{
+    $scope.resetGrid = () =>{
+        if(parentElement){
+            var nodes = parentElement.children();
+            for(var i = 0 ;i<nodes.length ; i++ ){
+                var node = nodes[i];
+                if(node.type == "button"){
+                    node.className = "button";
+                }
+            }
+        }
+    };
+
+    $scope.addInput = (event, index) => {
+        
         $scope.playable = true;
-        if($scope.ininialVal == 'O'){
+        if ($scope.ininialVal == 'O') {
             $scope.ininialVal = 'X';
-        }else if($scope.ininialVal == 'X'){
+        } else if ($scope.ininialVal == 'X') {
             $scope.ininialVal = 'O';
         }
-        $scope.inputArr[index-1] = $scope.ininialVal;
-        $scope.win = checkWin($scope.inputArr);
-        console.log($scope.inputArr);
-        console.log($scope.win);
+        $scope.inputArr[index - 1] = $scope.ininialVal;
+        $scope.win = checkWin($scope.inputArr, $scope.ininialVal);
+        var element = angular.element(event.target);
+        if(!parentElement){
+            parentElement = element.parent();
+        }
     }
 
 });
 
-function checkWin(arr){
-    return (checkHorizontal(arr));
-}
-
-function checkHorizontal(arr){
-    if(arr[0]==arr[1]==arr[2]=='X' || arr[0]==arr[1]==arr[2]=='O'){
-        return true;
-    }else if(arr[3]==arr[4]==arr[5]=='X' ||  arr[3]==arr[4]==arr[5]=='O'){
-        return true;
-    }else if(arr[6]==arr[7]==arr[8]=='X' || arr[6]==arr[7]==arr[8]=='O'){
+function checkWin(arr, initval) {
+    if(checkHorizontal(arr, 'X') || checkHorizontal(arr, 'O') ||
+        checkVertical(arr, 'X') || checkVertical(arr, 'O') ||
+        checkDigonal(arr, 'X') || checkDigonal(arr, 'O') ){
+        //alert(initval + "  Wins !!!");
         return true;
     }else{
         return false;
     }
 }
 
-function checkVertical(arr){
-    if(arr[0]==arr[3]==arr[6]=='X' || arr[0]==arr[3]==arr[6]=='O'){
+function checkHorizontal(arr, xory) {
+    if (arr[0] == xory && arr[1] == xory && arr[2] == xory) {
+        displayResultGrid(parentElement, [0,1,2]);
         return true;
-    }else if(arr[1]==arr[4]==arr[7]=='X' || arr[1]==arr[4]==arr[7]=='O'){
+    } else if (arr[3] == xory && arr[4] == xory && arr[5] == xory) {
+        displayResultGrid(parentElement, [3,4,5]);
         return true;
-    }else if(arr[2]==arr[5]==arr[8]=='X' || arr[2]==arr[5]==arr[8]=='O'){
+    } else if (arr[6] == xory && arr[7] == xory && arr[8] == xory) {
+        displayResultGrid(parentElement, [6,7,8]);
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function checkDigonal(arr){
-    if(arr[0]==arr[4]==arr[8]=='X' || arr[0]==arr[4]==arr[8]=='O'){
+function checkVertical(arr, xory) {
+    if (arr[0] == xory && arr[3] == xory && arr[6] == xory) {
+        displayResultGrid(parentElement, [0,3,6]);
         return true;
-    }else if(arr[2]==arr[4]==arr[6]=='X' || arr[2]==arr[4]==arr[6]=='O'){
+    } else if (arr[1] == xory && arr[4] == xory && arr[7] == xory) {
+        displayResultGrid(parentElement, [1,4,7]);
         return true;
-    }else{
+    } else if (arr[2] == xory && arr[5] == xory && arr[8] == xory) {
+        displayResultGrid(parentElement, [2,5,8]);
+        return true;
+    } else {
         return false;
+    }
+}
+
+function checkDigonal(arr, xory) {
+    if (arr[0] == xory && arr[4] == xory && arr[8] == xory) {
+        displayResultGrid(parentElement, [1,4,8]);
+        return true;
+    } else if (arr[2] == xory && arr[4] == xory && arr[6] == xory) {
+        displayResultGrid(parentElement, [2,4,6]);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function displayResultGrid(parentNode, positions){
+    console.log(parentNode);
+    var nodes = parentNode.children();
+    for(var i = 0 ;i<nodes.length ; i++ ){
+        var node = nodes[i];
+        if(node.type == "button" && positions.includes(i)){
+            node.className = "win-button";
+        }
+    }
+        
+}
+
+function resetGrid(){
+    if(parentElement){
+        var nodes = parentElement.children();
+        for(var i = 0 ;i<nodes.length ; i++ ){
+            var node = nodes[i];
+            if(node.type == "button"){
+                node.className = "button";
+            }
+        }
     }
 }
